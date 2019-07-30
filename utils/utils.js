052252377382b13eec4456fa8a671d6deac6d2a0
Shawn Tabrizi <shawntabrizi@gmail.com>
2019-07-30 17:19:37 +0200

    Add more utils

--- utils/utils.js
+++ utils/utils.js
@@ -24,3 +24,64 @@ function hex2bn() {
 		console.error(e);
 	}
 }
+
+/* AccountId to Hex */
+let a2h = {
+	"account": document.getElementById("account-a2h"),
+	"hex": document.getElementById("hex-a2h")
+};
+
+a2h.account.addEventListener("input", account2hex);
+a2h.hex.addEventListener("input", hex2account);
+
+function account2hex() {
+	try {
+		a2h.hex.value = keyring.decodeAddress(a2h.account.value);
+	} catch(e) {
+		a2h.hex.value = "Error";
+		console.error(e);
+	}
+}
+
+function hex2account() {
+	try {
+		a2h.account.value = keyring.encodeAddress(a2h.hex.value);
+	} catch(e) {
+		a2h.account.value = "Error";
+		console.error(e);
+	}
+}
+
+/* Blake-256 a String */
+let b256 = {
+	"input": document.getElementById("input-b256"),
+	"hash": document.getElementById("hash-b256")
+};
+
+b256.input.addEventListener("input", blake2string);
+
+function blake2string() {
+	try {
+		b256.hash.innerText = util_crypto.blake2AsHex(b256.input.value, 256);
+	} catch(e) {
+		b256.hash.innerText = "Error";
+		console.error(e);
+	}
+}
+
+/* Blake-256 a String */
+let xx128 = {
+	"input": document.getElementById("input-xx128"),
+	"hash": document.getElementById("hash-xx128")
+};
+
+xx128.input.addEventListener("input", xxhash2string);
+
+function xxhash2string() {
+	try {
+		xx128.hash.innerText = util_crypto.xxhashAsHex(xx128.input.value, 128);
+	} catch(e) {
+		xx128.hash.innerText = "Error";
+		console.error(e);
+	}
+}
